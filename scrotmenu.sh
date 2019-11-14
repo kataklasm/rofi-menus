09adf15565d9be770a5f1e41d113533a40614947
marius cramer <marcramer@protonmail.com>
2019-11-14 14:18:38 +0100

    inital commit

---
+++ scrotmenu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# options to be displayed
+option0="screen"
+option1="area"
+option2="window"
+
+# options to be displyed
+options="$option0\n$option1\n$option2"
+
+selected="$(echo -e "$options" | rofi -lines 3 -dmenu -p "scrot")"
+case $selected in
+    $option0)
+        sleep 1; scrot;;
+    $option1)
+        scrot -s;;
+    $option2)
+        sleep 1; scrot -u;;
+esac
