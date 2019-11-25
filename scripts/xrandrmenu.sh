16430e6cc926617065bac33c66a7c12793f82133
marius cramer <marcramer@protonmail.com>
2019-11-25 11:22:11 +0100

    generalize xrandrmenu.sh to ease adaption

--- scripts/xrandrmenu.sh
+++ scripts/xrandrmenu.sh
@@ -2,18 +2,18 @@
 
 # options to be displyed
 option0="activate all monitors"
-option1="monitor eDP-1 off"
-option2="monitor DP-1 off"
-option3="monitor eDP-1 primary"
-option4="monitor DP-1 primary"
-option5="monitor DP-1 same as 0"
-option6="monitor DP-1 right of 0"
-option7="monitor DP-1 left of 0"
-option8="monitor DP-1 above 0"
-option9="monitor DP-1 below 0"
-option10="rotate monitor DP-1 right"
-option11="rotate monitor DP-1 left"
-option12="rotate monitor DP-1 normal"
+option1="monitor <monitor0> off"
+option2="monitor <monitor1> off"
+option3="monitor <monitor0> primary"
+option4="monitor <monitor1> primary"
+option5="monitor <monitor1> same as <monitor0>"
+option6="monitor <monitor1> right of <monitor0>"
+option7="monitor <monitor1> left of <monitor0>"
+option8="monitor <monitor1> above <monitor0>"
+option9="monitor <monitor1> below <monitor0>"
+option10="rotate monitor <monitor1> right"
+option11="rotate monitor <monitor1> left"
+option12="rotate monitor <monitor1> normal"
 
 # Variable passed to rofi
 options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7\n$option8\n$option9\n$option10\n$option11\n$option12"
@@ -23,27 +23,27 @@ case $selected in
     $option0)
         xrandr --auto;;
     $option1)
-        xrandr --output eDP-1 --off;;
+        xrandr --output <monitor0> --off;;
     $option2)
-        xrandr --output DP-1 --off;;
+        xrandr --output <monitor1> --off;;
 	$option3)
-		xrandr --output eDP-1 --primary;;
+		xrandr --output <monitor0> --primary;;
 	$option4)
-		xrandr --output DP-1 --primary;;
+		xrandr --output <monitor1> --primary;;
     $option5)
-        xrandr --output DP-1 --same-as eDP-1;;
+        xrandr --output <monitor1> --same-as <monitor0>;;
     $option6)
-        xrandr --output DP-1 --right-of eDP-1;;
+        xrandr --output <monitor1> --right-of <monitor0>;;
     $option7)
-        xrandr --output DP-1 --left-of eDP-1;;
+        xrandr --output <monitor1> --left-of <monitor0>;;
     $option8)
-        xrandr --output DP-1 --above eDP-1;;
+        xrandr --output <monitor1> --above <monitor0>;;
     $option9)
-        xrandr --output DP-1 --below eDP-1;;
+        xrandr --output <monitor1> --below <monitor0>;;
     $option10)
-        xrandr --output DP-1 --rotate left;;
+        xrandr --output <monitor1> --rotate left;;
     $option11)
-        xrandr --output DP-1 --rotate right;;
+        xrandr --output <monitor1> --rotate right;;
     $option12)
-        xrandr --output DP-1 --rotate normal;;
+        xrandr --output <monitor1> --rotate normal;;
 esac
