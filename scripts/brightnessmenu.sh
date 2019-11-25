b0adcbfc65dc085ef1e7957af0832a3c4c6bc093
marius cramer <marcramer@protonmail.com>
2019-11-25 11:21:19 +0100

    generalize brightnessmenu.sh to ease adaption

--- scripts/brightnessmenu.sh
+++ scripts/brightnessmenu.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 # options do be displayed
-option0="min"
-option1="low"
-option2="medium"
-option3="high"
-option4="max"
+option0="two %"
+option1="twenty %"
+option2="forty %"
+option3="sixty %"
+option4="eighty %"
+option5="hundred %"
 
 # options passed to variable
-options="$option0\n$option1\n$option2\n$option3\n$option4"
+options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5"
 
 selected="$(echo -e "$options" | rofi -lines 5 -dmenu -p "brightness")"
 case $selected in
     $option0)
-		sudo bash -c "echo 50 > /sys/class/backlight/intel_backlight/brightness";;
+		sudo bash -c "echo <2%> > /sys/class/backlight/intel_backlight/brightness";;
     $option1)
-        sudo bash -c "echo 142 > /sys/class/backlight/intel_backlight/brightness";;
+        sudo bash -c "echo <20%> > /sys/class/backlight/intel_backlight/brightness";;
     $option2)
-        sudo bash -c "echo 234 > /sys/class/backlight/intel_backlight/brightness";;
+        sudo bash -c "echo <40%> > /sys/class/backlight/intel_backlight/brightness";;
 	$option3)
-        sudo bash -c "echo 325 > /sys/class/backlight/intel_backlight/brightness";;
+        sudo bash -c "echo <60%> > /sys/class/backlight/intel_backlight/brightness";;
 	$option4)
-        sudo bash -c "echo 416 > /sys/class/backlight/intel_backlight/brightness";;
+        sudo bash -c "echo <80%> > /sys/class/backlight/intel_backlight/brightness";;
+	$option5)
+        sudo bash -c "echo <100%> > /sys/class/backlight/intel_backlight/brightness";;
 esac
